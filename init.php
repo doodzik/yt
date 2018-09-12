b705930dd77e9bf14c25f9123833a72916103339
Frederik Dudzik <frederik@dudzik.co>
2018-09-12 05:03:36 +0200

    add correct vendor path when in deployment

--- init.php
+++ init.php
@@ -1,9 +1,6 @@
 <?php
-if (!file_exists(__DIR__ . '/vendor/autoload.php')) {
-  throw new \Exception('please run "composer install" in parent dir ' . __DIR__);
-}
 
-require_once __DIR__ . '/vendor/autoload.php';
+require __DIR__ . '/../env.php';
 
 function is_production() {
   return getenv("PRODUCTION") !== false;
@@ -13,6 +10,16 @@ if(!is_production()) {
   error_reporting(E_ALL);
   ini_set('display_errors', 1);
   ini_set('display_startup_errors', 1);
+
+  if (!file_exists(__DIR__ . '/vendor/autoload.php')) {
+    throw new \Exception('please run "composer install" in parent dir ' . __DIR__);
+  }
+  require_once __DIR__ . '/vendor/autoload.php';
+} else {
+  if (!file_exists(__DIR__ . '/../vendor/autoload.php')) {
+    throw new \Exception('please run "composer install" in parent dir ' . __DIR__);
+  }
+  require_once __DIR__ . '/../vendor/autoload.php';
 }
 
 require('lib/youtube.php');
